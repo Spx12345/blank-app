f60b299a1bbe33b1af597d5b93d2b7e247ea762c
Spx12345 <siapohxiang040328@gmail.com>
2025-11-11 09:56:35 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,5 +23,5 @@ env.run()
 result = []
 for fact in env.facts():
   if fact.template.name == 'result':
-    results.append(fact[;name;]) # why assert the fact?
+    results.append(fact['name']) # why assert the fact?
 st.write(results[0],"better output")
